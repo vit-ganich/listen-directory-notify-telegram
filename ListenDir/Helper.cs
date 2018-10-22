34b3442c7dcb031d54213caf18ac15d675dd172c
vit-ganich <vit-ganich@yandex.ru>
2018-10-22 16:24:10 +0300

    Updates

--- ListenDir/Helper.cs
+++ ListenDir/Helper.cs
@@ -19,13 +19,23 @@ namespace TestResultsReminder
         public static void DisplayGeneralInfo()
         {
             Console.WriteLine("------------------- Configuration info -------------------");
-            Console.WriteLine($"Directory: {ConfigReader.GetTestResultsDir()}");
+            DisplayFoldersForListen();
             Console.WriteLine($"Files extension: {ConfigReader.GetFilesExtension().ToUpper()}");
             Console.WriteLine($"Search timeout: {ConfigReader.GetSearchTimeout()/1000} seconds");
             Console.WriteLine($"Telegram message from user: +{ConfigReader.GetUserPhoneNumber()}");
             Console.Write($"Telegram message to {ConfigReader.GetRecipientType()}: ");
             Console.WriteLine(GetRecipientInfo());
             Console.WriteLine("----------------------------------------------------------\n");
+        }
+
+        public static void DisplayFoldersForListen()
+        {
+            Console.Write("Folders to listen: ");
+            foreach(var folder in ConfigReader.GetTestResultsDir())
+            {
+                Console.Write(folder + ", ");
+            }
+            Console.WriteLine();
         }
 
         /// <summary>
